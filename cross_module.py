a156763af3bc2114d0305e1c073dd11e46bf4c0a
hongji <xingkongdewuyu@icloud.com>
2023-12-17 22:59:50 +0900

    add cross_module

--- cross_module.py
+++ cross_module.py
@@ -5,5 +5,6 @@ def cross(azimuth, ground_range, height, amp, ground_range_star, ground_range_en
     azimuth = azimuth[indices[1]]
     ground_range = ground_range[indices]
     height = height[indices]
+    amp = np.array(amp)
     amp = amp[indices[1]]
     return azimuth, ground_range, height, amp
